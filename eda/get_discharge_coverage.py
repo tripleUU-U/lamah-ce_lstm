95d7befbdd352c5fe953f0bfeae5dd2a40721de9
Wenzel Uhlmann <wenzel.uhlmann@gmx.de>
2025-09-19 16:53:31 +0000

    adapted counting of normal periods and fixed bug (where q5q95 got the gaps, which explained the extreme scarcity of data)
    now 8 *10e6 samples are available from periods with at least 30 days of q between q5 and q95

--- eda/get_discharge_coverage.py
+++ eda/get_discharge_coverage.py
@@ -15,7 +15,13 @@ def main():
     # Static attribute file is needed to filter out basins, without static catchment attributes.
     static_attributes_path = data_directory_path / "raw_data/2_LamaH-CE_daily/B_basins_intermediate_all/1_attributes/Catchment_attributes.csv"
     static_attributes = pd.read_table(static_attributes_path, header=0, sep=";")
-    valid_basin_ids = list(static_attributes["ID"])
+    static_attributes.set_index("ID", inplace=True)
+    valid_basin_ids = list(static_attributes.index)
+
+    # Get Hydrological indices. 
+    hydro_indices_path = data_directory_path / "raw_data/2_LamaH-CE_daily/D_gauges/1_attributes/Hydro_indices_1981_2017.csv"
+    hydro_indices = pd.read_table(hydro_indices_path, header=0, sep=";")
+    hydro_indices.set_index("ID", inplace=True)
 
     # Gather path objects of all discharge timeseries. 
     discharge_timeseries_paths = list(sorted(discharge_data_path.glob("*.csv")))
@@ -32,6 +38,10 @@ def main():
     average_gap_lengths = []
     max_gap_lengths = []
     gap_dates = []
+
+    # Normal periods.
+    q5_q95_normal_periods = []
+    z_normal_periods = []
 
     for path in discharge_timeseries_paths:
 
@@ -73,14 +83,39 @@ def main():
         current_gap_len = 0
         current_gap_date_range = [None, None]
 
+        # Prepare tracking of periods between Q5 and Q95. 
+        q5 = hydro_indices.loc[basin_id, "Q5"] 
+        q95 = hydro_indices.loc[basin_id, "Q95"]
+
+        # Q5 and Q95 are averaged over the area, revert.
+        area = static_attributes.loc[basin_id, "area_calc"]
+        q5 = (q5 * area * 10e6) / (1000* 86400)
+        q95 = (q95 * area * 10e6) / (1000 * 86400)
+        
+        current_normal_period_duration = 0 
+        current_normal_period_date_span = [None, None]
+
+        basin_q_normal_periods = []
+        
+        # Calculate mean and std for z-score. 
+        q_mean = discharge_timeseries["qobs"].mean()
+        q_std = discharge_timeseries["qobs"].std()
+
+        current_z_duration = 0 
+        current_z_date_span = [None, None]
+
+        basin_z_normal_periods = []
+
         for day in discharge_timeseries.index:
 
-            if discharge_timeseries.loc[day, "qobs"] == -999: 
+            qobs_day = discharge_timeseries.loc[day, "qobs"]
+
+            if qobs_day == -999: 
 
                 # If there is no current gap but a NA, start a new one. 
                 if current_gap_len == 0: 
 
-                    current_gap_date_range[0] = day
+                    current_gap_date_range[0] = str(day)
 
                 current_gap_len += 1
 
@@ -88,7 +123,7 @@ def main():
                 if day == discharge_timeseries.index[ts_len-1]:
 
                     # Reset not necessary, since its the last day. 
-                    current_gap_date_range[1] = day
+                    current_gap_date_range[1] = str(day)
                     gap_date_ranges.append(current_gap_date_range)
                     
             else: 
@@ -99,11 +134,53 @@ def main():
                     current_gap_len = 0
 
                     # Also add the prior date, as the last gap day.
-                    current_gap_date_range[1] = day - pd.Timedelta(days=1)
+                    current_gap_date_range[1] = str(day - pd.Timedelta(days=1))
 
                     gap_date_ranges.append(current_gap_date_range)
                     current_gap_date_range = [None, None]
         
+            z_score = (qobs_day - q_mean) / q_std
+
+            # Track z score normal periods. 
+            if abs(z_score) < 2.0: 
+
+                # Z Normal period starts. 
+                if current_z_duration == 0: 
+                    current_z_date_span[0] = day
+
+                current_z_duration += 1
+
+            else: 
+
+                # If there was a normal period, its now over. 
+                if current_z_duration > 0: 
+                    current_z_date_span[1] = day - pd.Timedelta(days=1)
+                    
+                    # Append normal period to list.
+                    basin_z_normal_periods.append(current_z_date_span) 
+                
+                current_z_duration = 0 
+                current_z_date_span = [None, None]
+
+            # Track "normal" periods with q5 < qobs < q95. 
+            if qobs_day > q5 and qobs_day < q95: 
+
+                # Normal period starts. 
+                if current_normal_period_duration == 0: 
+                    current_normal_period_date_span[0] = day
+
+                current_normal_period_duration += 1
+            else: 
+                # If there was a normal period, its now over. 
+                if current_normal_period_duration > 0: 
+                    current_normal_period_date_span[1] = day - pd.Timedelta(days=1)
+                    
+                    if current_normal_period_duration >= 0:
+                        basin_q_normal_periods.append(current_normal_period_date_span)
+                
+                current_normal_period_duration = 0 
+                current_normal_period_date_span = [None, None]
+
         if gap_lengths: 
             average_gap_lengths.append(np.mean(gap_lengths))
             max_gap_lengths.append(np.max(gap_lengths))
@@ -114,7 +191,17 @@ def main():
             max_gap_lengths.append(0)
             gap_dates.append([])
 
-        # Create basin states df to display in logger. 
+        if basin_z_normal_periods:
+            z_normal_periods.append(basin_z_normal_periods)
+        else:
+            z_normal_periods.append([])
+
+        if basin_q_normal_periods:
+            q5_q95_normal_periods.append(basin_z_normal_periods)
+        else:
+            q5_q95_normal_periods.append([])
+
+        # Create basin stats df to display in logger. 
         basin_stats = pd.DataFrame([{
             "basin_id": basin_id,
             "timeseries_length": ts_len,
@@ -124,7 +211,9 @@ def main():
             "percentage [%]": gap_percentages[-1],
             "average_gap_length": average_gap_lengths[-1],
             "max_gap_length": max_gap_lengths[-1],
-            "gap_dates": gap_dates[-1]
+            "gap_dates": gap_dates[-1],
+            "z_normal_period": z_normal_periods[-1],
+            "q5_q95": q5_q95_normal_periods[-1]
         }])
         logger.info("\n%s", basin_stats)
 
@@ -139,18 +228,20 @@ def main():
         "percentage [%]": gap_percentages,
         "average_gap_length": average_gap_lengths,
         "max_gap_length": max_gap_lengths,
-        "gap_dates": gap_dates
+        "gap_dates": gap_dates,
+        "q5_q95_normal_periods": q5_q95_normal_periods,
+        "z_normal_periods": z_normal_periods 
     })
 
     # Sort after basin id and set as index.
     coverage_stats_sorted = coverage_stats.sort_values("basin_id")
     coverage_stats_sorted.set_index("basin_id", inplace=True)
 
+
     # Write to disk.
     processed_data_path = Path("/home/wuhlmann/BA/data/processed_data")
-    coverage_stats_sorted.to_csv(
-        processed_data_path / "eda/discharge_coverage.csv",
-        sep=";"
+    coverage_stats_sorted.to_pickle(
+        processed_data_path / "eda/discharge_coverage.pkl"
     )
 
 if __name__ == "__main__": 
